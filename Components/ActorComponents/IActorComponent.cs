cde375aab8a3ff7a426b04c8f80ff76c62be8fe7
AtinYo <18202408637@163.com>
2018-04-04 15:30:53 +0800

    Add ActorComponent

---
+++ Components/ActorComponents/IActorComponent.cs
@@ -0,0 +1,16 @@
+﻿/************************************************************
+     File      : IActorComponent.cs
+     brief     : Basic actor component.
+     author    : Atin
+     version   : 1.0
+     date      : 2018/04/04 14:00:00
+     copyright : 2018, Atin. All rights reserved.
+**************************************************************/
+using CES;
+namespace ActorComponents
+{
+    public class IActorComponent: IComponent
+    {
+        
+    }
+}
